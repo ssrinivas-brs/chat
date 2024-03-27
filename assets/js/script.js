0adc314f409693f9481b82b3ab9dadc7dc6479dc
ssrinivas-brs <144874882+ssrinivas-brs@users.noreply.github.com>
2024-03-27 12:49:26 +0530

    add emojis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,127 @@
-
-
 function loadScript() {
   let user = prompt("Please enter your name", "");
   if (!user) {
-    document.getElementsByTagName('main')[0].innerHTML = 'Please Enter Your Name';
+    document.getElementsByTagName("main")[0].innerHTML =
+      "Please Enter Your Name";
   }
 
   var pubnub = new PubNub({
-      publishKey: 'demo',
-      subscribeKey: 'demo',
-      userId: user
-  })
+    publishKey: "demo",
+    subscribeKey: "demo",
+    userId: user,
+  });
 
   pubnub.subscribe({
-      channels: ['ws-channel']
-  })
+    channels: ["ws-channel"],
+  });
 
   pubnub.addListener({
-    message: payload => {
-      let messageString = '';
+    message: (payload) => {
+      let messageString = "";
       const isCurrentUserPublisher = user === payload.publisher;
-      const msg_cls = isCurrentUserPublisher ? 'justify-content-md-end' : 'justify-content-md-start';
-      if(isCurrentUserPublisher) {
-        messageString =
-        `
+      const msg_cls = isCurrentUserPublisher
+        ? "justify-content-md-end"
+        : "justify-content-md-start";
+      if (payload.messageType === "image") {
+        messageString = `
           <div class='row w-100 ${msg_cls} mb-2'>
             <div class="col-5 text-start sender-msg p-0">
-              <div class='d-inline'>${payload.message}</div>
+              <img src="${payload.messages}" class="message-image" />
             </div>
             <div class="col-1 p-0">
               <div class='d-inline text-dark fw-bold'>${payload.publisher}</div>
             </div>
           </div>
-        `
+        `;
       } else {
-        messageString =
-        `
-          <div class='row w-100 ${msg_cls} mb-2'>
-            <div class="col-1 p-0">
-              <div class='d-inline text-dark fw-bold'>${payload.publisher}</div>
+        if (isCurrentUserPublisher) {
+          messageString = `
+            <div class='row w-100 ${msg_cls} mb-2'>
+              <div class="col-5 text-start sender-msg p-0">
+                <div class='d-inline'>${payload.message}</div>
+              </div>
+              <div class="col-1 p-0">
+                <div class='d-inline text-dark fw-bold'>${payload.publisher}</div>
+              </div>
             </div>
-            <div class="col-5 text-start receiver-msg p-0">
-              <div class='d-inline'>${payload.message}</div>
+          `;
+        } else {
+          messageString = `
+            <div class='row w-100 ${msg_cls} mb-2'>
+              <div class="col-1 p-0">
+                <div class='d-inline text-dark fw-bold'>${payload.publisher}</div>
+              </div>
+              <div class="col-5 text-start receiver-msg p-0">
+                <div class='d-inline'>${payload.message}</div>
+              </div>
             </div>
-          </div>
-        `
+          `;
+        }
       }
-      document.getElementById('messages').innerHTML += messageString;
-    }
-  })
+      document.getElementById("messages").innerHTML += messageString;
+    },
+  });
 
   function sendMessage(event) {
-    var inputMessage = document.getElementById('message')
-    if(inputMessage.value) {
+    var inputMessage = document.getElementById("message");
+    var messages = document.getElementById("messages");
+    var fileInput = document.getElementById("file-input");
+
+    if (fileInput.files.length > 0) {
+      var file = fileInput.files[0];
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        var imageUrl = event.target.result;
+        var img = document.createElement("img");
+        img.classList.add("message-image");
+        img.src = imageUrl;
+
+        var imageContent = document.createElement("div");
+        imageContent.classList.add("image-content");
+
+        var imageContainer = document.createElement("div");
+        imageContainer.classList.add("image-container");
+        imageContent.appendChild(imageContainer);
+        imageContainer.appendChild(img);
+        messages.appendChild(imageContent);
+
+        pubnub.publish({
+          channel: "ws-channel",
+          message: messages.value,
+        });
+      };
+
+      reader.readAsDataURL(file);
+
+      fileInput.value = "";
+    } else if (inputMessage.value) {
       pubnub.publish({
-        channel: 'ws-channel',
-        message: inputMessage.value
-      })
-      inputMessage.value = ""
-      event.preventDefault();
+        channel: "ws-channel",
+        message: inputMessage.value,
+      });
+      inputMessage.value = "";
     }
+
+    event.preventDefault();
   }
-  document.getElementById('input-form').addEventListener('submit', sendMessage);
+  document.getElementById("input-form").addEventListener("submit", sendMessage);
+}
+
+const emojiBtn = document.querySelector("#emoji-btn");
+const picker = new EmojiButton();
+// Emoji selection
+window.addEventListener("DOMContentLoaded", () => {
+  picker.on("emoji", (emoji) => {
+    document.querySelector("input").value += emoji;
+  });
+
+  emojiBtn.addEventListener("mouseover", () => {
+    picker.togglePicker(emojiBtn);
+  });
+});
+function setTheme(theme) {
+  document.documentElement.style.setProperty("--primary-color", theme);
+  localStorage.setItem("hpc-theme", theme);
 }
 
 window.onload = loadScript;
